2de2fc044d6b06b8dfcaf86bd1a3f666fa7addda
sonesonjabgo <thswogud02@gmail.com>
2024-07-16 18:59:56 +0900

    fix: 상세페이지 metadata

--- app/( movies)/movies/[id]/page.tsx
+++ app/( movies)/movies/[id]/page.tsx
@@ -1,12 +1,19 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export default function MovieDetail({
-  params: { id },
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+export async function generateMetaData({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default function MovieDetail({ params: { id } }: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading Movie Video</h1>}>
@@ -18,3 +25,9 @@ export default function MovieDetail({
     </div>
   );
 }
+
+/**
+ * 코드 챌린지:
+ * /credit, /providers, /similar url 사용해서
+ * 상세 페이지 예쁘게 꾸며보기
+ */
